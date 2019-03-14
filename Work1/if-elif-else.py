d73406da23549bfd2abf256ebac37460d18e87d8
Poplavskikh <poplavskikh.iuo@gmail.com>
2019-03-14 21:10:07 +0500

    Adding new files

---
+++ Work1/if-elif-else.py
@@ -0,0 +1,9 @@
+# Value assignment
+guess_me = 7
+# Checking the value of a variable
+if guess_me < 7:
+    print("too low")
+elif guess_me > 7:
+    print("too high")
+else:
+    print("just right")
